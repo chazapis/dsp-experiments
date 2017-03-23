8af280964da671e13d211f78167e8e864a37a440
Antony Chazapis <chazapis@gmail.com>
2017-03-23 22:40:54 +0200

    Turn NCO into a class

--- nco.py
+++ nco.py
@@ -1,63 +1,53 @@
 #!/usr/bin/env python
 
-import sys
-
 from math import sin, cos, pi
 
 SAMPLING_RATE = 2 * (10 ** 6) # 2 Msps
-
 ACCUMULATOR_BITS = 32
 LOOKUP_TABLE_INDEX_BITS = 18 # keep smaller than ACCUMULATOR_BITS
 
-accumulator = 0
+class NCO(object):
+    def __init__(self,
+                 sampling_rate=SAMPLING_RATE,
+                 accumulator_bits=ACCUMULATOR_BITS,
+                 lookup_table_index_bits=LOOKUP_TABLE_INDEX_BITS):
+        self.sampling_rate = sampling_rate
+        self.accumulator_bits = accumulator_bits
+        self.lookup_table_index_bits = lookup_table_index_bits
 
-lookup_table = []
-lookup_table_size = 2 ** LOOKUP_TABLE_INDEX_BITS
-for i in range(lookup_table_size):
-    lookup_table.append(sin((2 * pi * i) / lookup_table_size))
+        self.lookup_table = []
+        self.lookup_table_size = 2 ** self.lookup_table_index_bits
+        for i in range(self.lookup_table_size):
+            self.lookup_table.append(sin((2 * pi * i) / self.lookup_table_size))
 
-frequency = 20 * (10 ** 6) # 20 MHz 
-frequency = 5000
+        self.sample_time = 1.0 / self.sampling_rate
+        
+        self.accumulator_size = 2 ** self.accumulator_bits
+        self.accumulator_shift = self.accumulator_bits - self.lookup_table_index_bits
+
+        self.accumulator = 0
+        
+    def run(self, frequency, seconds):
+        phase_per_sample = int(self.accumulator_size * frequency * self.sample_time)
+        while seconds >= 0:
+            seconds -= self.sample_time
+
+            self.accumulator += phase_per_sample
+            self.accumulator %= self.accumulator_size # overflow
+
+            yield self.lookup_table[self.accumulator >> self.accumulator_shift]
 
 if __name__ == '__main__':
-    seconds = 0.02
+    import argparse
 
-    sample_time = 1.0 / SAMPLING_RATE
-    accumulator_size = 2 ** ACCUMULATOR_BITS
-    accumulator_shift = ACCUMULATOR_BITS - LOOKUP_TABLE_INDEX_BITS
-    phase_per_sample = int(accumulator_size * frequency * sample_time)
-    while seconds >= 0:
-        seconds -= sample_time
+    parser = argparse.ArgumentParser(description='Numerically Controlled Oscillator (accumulator bits: %d, lookup table index bits: %d)' % (ACCUMULATOR_BITS, LOOKUP_TABLE_INDEX_BITS))
+    parser.add_argument('frequency', type=int, nargs='?', default=5000)
+    parser.add_argument('seconds', type=float, nargs='?', default=0.01)
+    args = parser.parse_args()
 
-        accumulator += phase_per_sample
-        accumulator %= accumulator_size # overflow
-        
-        print lookup_table[accumulator >> accumulator_shift]
+    frequency = args.frequency
+    seconds = args.seconds
 
-    # for i in range(lookup_table_size):
-    #     print lookup_table[i]
-    # sys.exit(0)
-
-    # print 'Numerically Controlled Oscillator'
-    # print '(accumulator bits: %d, lookup table index bits: %d)' % (ACCUMULATOR_BITS, LOOKUP_TABLE_INDEX_BITS)
-    # print
-
-    # while True:
-    #     try:
-    #         frequency = raw_input('Enter frequency: ')
-    #         if not frequency.isdigit():
-    #             raise ValueError
-    #         frequency = int(frequency)
-
-    #         seconds = raw_input('Enter time in seconds: ')
-    #         if not seconds.isdigit():
-    #             raise ValueError
-    #         seconds = int(seconds)
-
-    #         print 'Running for frequency: %d, seconds: %d' % (frequency, seconds)
-
-    #     except ValueError:
-    #         print 'Wrong input'
-    #     except (KeyboardInterrupt, EOFError):
-    #         break
-
+    nco = NCO()
+    for v in nco.run(frequency, seconds):
+        print v
